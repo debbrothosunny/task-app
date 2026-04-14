8a193e85badb07df8a61f7ceae095d1661f6c210
debbrothosunny <debnathsunny7852@gmail.com>
2026-04-14 16:56:00 +0600

    Update cors.php for railway production

--- config/cors.php
+++ config/cors.php
@@ -3,22 +3,22 @@
 return [
 
     'paths' => [
-    'api/*', 
-    'sanctum/csrf-cookie', 
-    'login', 
-    'logout', 
-    'register' 
+        'api/*', 
+        'sanctum/csrf-cookie', 
+        'login', 
+        'logout', 
+        'register' 
     ],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
+    'allowed_origins' => array_filter([  // array_filter removes null values
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'http://localhost:8000',    
         'http://127.0.0.1:8000',
-        env('FRONTEND_URL'),
-    ],
+        env('FRONTEND_URL'),         // make sure this is set in Railway!
+    ]),
 
     'allowed_origins_patterns' => [],
 
